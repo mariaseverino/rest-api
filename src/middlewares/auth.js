0a7338025ce1f5367e389e8cb36e5969ee807e7a
Maria Severino <mariarsseverino@gmail.com>
2020-07-10 13:13:45 -0300

    🔧 fix an error

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -10,7 +10,7 @@ module.exports = (req, res, next) => {
 
     const parts = authHeader.split(' ');
 
-    if (!parts.length === 2){
+    if (!(parts.length === 2)){
         return res.status(401).send({error: 'Token error'})
     }
     
